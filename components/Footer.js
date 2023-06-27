9a04d1a9323a9aa30aeaf8ace03f8d38c208f52a
Kartik Awadh Yadav <120031887+Kartikgc9@users.noreply.github.com>
2023-06-27 17:01:59 +0530

    changes

---
+++ components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p className="text-center">
+          Made by KAY and Next.js
+        </p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
